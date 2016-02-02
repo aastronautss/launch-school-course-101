1ba1073367da7e2ce013a72318c6331649d9e8ce
Tyler Guillen <tyguillen@gmail.com>
2016-02-02 09:11:09 -0500

    clean up files, add documentation for twenty-one

--- lesson-4/twenty-one.rb
+++ lesson-4/twenty-one.rb
@@ -4,15 +4,18 @@ CARD_NAMES = { 2 => "2", 3 => "3", 4 => "4", 5 => "5", 6 => "6",
 SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 VALUES = (2..14).to_a
 
+# Standard output
 def prompt(message)
   puts "=> #{message}"
 end
 
+# Standard list of elements.
 def joinor(array, delimiter = ', ', word = 'or')
   array[-1] = "#{word} #{array[-1]}" if array.length > 1
   array.join delimiter
 end
 
+# Returns a deck of card "objects" in the form of an array of hashes.
 def initialize_deck
   deck = []
 
@@ -25,6 +28,7 @@ def initialize_deck
   deck.shuffle
 end
 
+# Pops two cars off the top of the given deck and returns those two cards.
 def deal!(deck)
   hand = []
   hand << deck.pop
@@ -32,14 +36,18 @@ def deal!(deck)
   hand
 end
 
+# If the card is above 11 (i.e. a face card), returns 10.
 def evaluate_non_ace_card(card_value)
   card_value < 11 ? card_value : 10
 end
 
+# Used to evaluate an Ace card based on the given sum
+# (Avoids busting if possible)
 def one_or_eleven(sum)
   sum + 11 > 21 ? 1 : 11
 end
 
+# Takes a hand and counts up its value. Avoids busting if possible.
 def evaluate_hand(hand)
   aces = hand.select { |card| card[:value] == 14 } # First take out aces
   hand_minus_aces = hand - aces
@@ -55,22 +63,28 @@ def evaluate_hand(hand)
   sum
 end
 
+# Returns true if the given hand's value is over 21.
 def bust?(hand)
   evaluate_hand(hand) > 21
 end
 
+# Returns the winning hand (defaults to dealer in the case of a tie)
 def get_winner(player_hand, dealer_hand)
   evaluate_hand(player_hand) > evaluate_hand(dealer_hand) ? 'player' : 'dealer'
 end
 
+# Pops a card from the deck and places it in the hand.
 def hit!(deck, hand)
   hand << deck.pop
 end
 
+# Returns a string giving the name of the card.
 def name_of_card(card)
   "#{CARD_NAMES[card[:value]]} of #{card[:suit]}"
 end
 
+# Returns a string listing (in english) the names of each card in the
+# given hand.
 def list_cards(hand)
   list = hand.map do |card|
     name_of_card(card)
@@ -79,19 +93,24 @@ def list_cards(hand)
   joinor(list, ', ', 'and')
 end
 
+# Displays the cards in play in a readable format. Meant for the player's
+# turn.
 def show_hands_player_turn(player_hand, dealer_hand)
   prompt "Dealer has: #{name_of_card(dealer_hand.first)} and unknown card."
   prompt "Player has: #{list_cards(player_hand)}"
 end
 
+# Reveals the dealer's full hand.
 def dealer_reveal(hand)
   prompt "Dealer has: #{list_cards(hand)}"
 end
 
+# Input validation.
 def valid_input?(input)
   input == 's' || input == 'h'
 end
 
+# User input validation loop.
 def user_input
   input = ''
     loop do # Input validation
@@ -103,6 +122,7 @@ def user_input
     input
 end
 
+# Player's turn. Returns 'dealer' if the player busts.
 def player_turn!(deck, player_hand, dealer_hand)
   loop do
     show_hands_player_turn(player_hand, dealer_hand)
@@ -123,6 +143,7 @@ def player_turn!(deck, player_hand, dealer_hand)
   end
 end
 
+# Dealer's turn. Returns 'player' if the dealer busts.
 def dealer_turn!(deck, hand)
   dealer_reveal(hand)
 
@@ -137,6 +158,7 @@ def dealer_turn!(deck, hand)
   end
 end
 
+# Prints a readable format for the winner.
 def display_winner(winner)
   prompt "#{winner.capitalize} wins!"
 end
